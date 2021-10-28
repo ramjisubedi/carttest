56fcd80d3df3e14004bf38076907561fec72e19c
ramji subedi <ramjisubedi1991@gmail.com>
2021-10-28 20:17:30 +0545

    test khalti

--- application/views/cart/index.php
+++ application/views/cart/index.php
@@ -42,19 +42,62 @@ function updateCartItem(obj, rowid){
     <?php } ?> 
 <tr>
     <td><a href="<?= base_url('checkout') ?>">Checkout</a>
-
-        <form action="https://uat.esewa.com.np/epay/main" method="POST">
-    <input value="10023" name="tAmt" type="hidden">
-    <input value="9023" name="amt" type="hidden">
-    <input value="523" name="txAmt" type="hidden">
-    <input value="223" name="psc" type="hidden">
-    <input value="323" name="pdc" type="hidden">
+       <form action="https://uat.esewa.com.np/epay/main" method="POST">
+    <input value="100" name="tAmt" type="hidden">
+    <input value="90" name="amt" type="hidden">
+    <input value="0" name="txAmt" type="hidden">
+    <input value="2" name="psc" type="hidden">
+    <input value="3" name="pdc" type="hidden">
     <input value="EPAYTEST" name="scd" type="hidden">
     <input value="ee2c3ca1-696b-4cc5-a6be-2c40d929d453" name="pid" type="hidden">
     <input value="http://merchant.com.np/page/esewa_payment_success?q=su" type="hidden" name="su">
     <input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
     <input value="Pay with Esewa <?php echo 'Rs'.$item["subtotal"]; ?>" type="submit">
     </form>
+
+<script src="https://khalti.s3.ap-south-1.amazonaws.com/KPG/dist/2020.12.17.0.0.0/khalti-checkout.iffe.js"></script>
+    <!-- Place this where you need payment button -->
+    <button id="payment-button">Pay with Khalti <?php echo 'Rs'.$item["subtotal"]; ?></button>
+    <!-- Place this where you need payment button -->
+    <!-- Paste this code anywhere in you body tag -->
+    <script>
+        var config = {
+            // replace the publicKey with yours
+            "publicKey": "test_public_key_dc74e0fd57cb46cd93832aee0a390234",
+            "productIdentity": "1234567890",
+            "productName": "Dragon",
+            "productUrl": "http://gameofthrones.wikia.com/wiki/Dragons",
+            "paymentPreference": [
+                "KHALTI",
+                "EBANKING",
+                "MOBILE_BANKING",
+                "CONNECT_IPS",
+                "SCT",
+                ],
+            "eventHandler": {
+                onSuccess (payload) {
+                    // hit merchant api for initiating verfication
+                    console.log(payload);
+                },
+                onError (error) {
+                    console.log(error);
+                },
+                onClose () {
+                    console.log('widget is closing');
+                }
+            }
+        };
+
+        var checkout = new KhaltiCheckout(config);
+        var btn = document.getElementById("payment-button");
+        btn.onclick = function () {
+            // minimum transaction amount must be 10, i.e 1000 in paisa.
+            checkout.show({amount: 1000});
+        }
+    </script>
+
+
+
 
     </td>
 </tr>
